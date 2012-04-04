f94155fbdf7aa1b8538b89820d4f7b90cfda8167
Lars Vogel <Lars.Vogel@gmail.com>
2012-04-04 15:21:54 +0200

    Added Menu and Toolbar

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/ExitHandler.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/ExitHandler.java
@@ -2,10 +2,12 @@ package com.example.e4.rcp.todo.handlers;
 
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.workbench.IWorkbench;
 
 public class ExitHandler {
 	@Execute
-	public void execute() {
+	public void execute(IWorkbench workbench) {
+		workbench.close();
 	}
 
 	@CanExecute
