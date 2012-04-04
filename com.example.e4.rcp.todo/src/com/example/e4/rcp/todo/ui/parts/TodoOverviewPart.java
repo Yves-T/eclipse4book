eb000864d13e92be3dd24788fbe86f4da51a6085
Lars Vogel <Lars.Vogel@gmail.com>
2012-04-04 13:15:28 +0200

    First implementation of TodoOverviewPart

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/TodoOverviewPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/TodoOverviewPart.java
@@ -1,5 +1,19 @@
 package com.example.e4.rcp.todo.ui.parts;
 
+import javax.inject.Inject;
+
+import org.eclipse.swt.widgets.Composite;
+
 public class TodoOverviewPart {
 
+	@Inject
+	public TodoOverviewPart(Composite parent) {
+		if (parent != null) {
+			// Print the layout to the console
+			System.out.println("Got Composite via DI.");
+			System.out.println("Layout: " + parent.getLayout().getClass());
+		} else {
+			System.out.println("No Composite available.");
+		}
+	}
 }
