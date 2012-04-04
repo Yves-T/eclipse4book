fd2824af4ed18ad4a8f299b70ac1b1948660166a
Lars Vogel <Lars.Vogel@gmail.com>
2012-04-04 16:29:11 +0200

    Getting the command ID in the handler - example

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/SaveHandler.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/SaveHandler.java
@@ -2,11 +2,14 @@ package com.example.e4.rcp.todo.handlers;
 
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.commands.MCommand;
+import org.eclipse.e4.ui.model.application.ui.menu.MHandledItem;
 
 public class SaveHandler {
 	@Execute
-	public void execute() {
-		System.out.println("Called");
+	public void execute(MHandledItem item) {
+		MCommand command = item.getCommand();
+		System.out.println(command.getElementId());
 	}
 
 	@CanExecute
--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/TestHandler.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/TestHandler.java
@@ -2,10 +2,12 @@ package com.example.e4.rcp.todo.handlers;
 
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.menu.MItem;
 
 public class TestHandler {
 	@Execute
-	public void execute() {
+	public void execute(MItem item) {
+		System.out.println(item.getElementId());
 	}
 
 	@CanExecute
