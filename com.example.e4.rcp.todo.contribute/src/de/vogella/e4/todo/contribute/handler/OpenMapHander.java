843dd0d4c6c79b3bc3bb8dcd3306bf01f0ae324b
Lars Vogel <Lars.Vogel@gmail.com>
2012-04-18 00:00:16 +0200

    Created the contribution plug-in

---
+++ com.example.e4.rcp.todo.contribute/src/de/vogella/e4/todo/contribute/handler/OpenMapHander.java
@@ -0,0 +1,14 @@
+
+package de.vogella.e4.todo.contribute.handler;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class OpenMapHander {
+
+	@Execute 
+	public void run(Shell shell){
+		MessageDialog.openInformation(shell, "Just testing", "This is just a test");
+	}
+}
