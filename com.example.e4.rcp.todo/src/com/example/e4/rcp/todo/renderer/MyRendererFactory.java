8e61a49d0c40b4eabe4d0f48d9744285e7a53e7f
Lars Vogel <Lars.Vogel@gmail.com>
2012-05-11 08:16:17 +0200

    created a new renderer for MPart

---
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/renderer/MyRendererFactory.java
@@ -0,0 +1,27 @@
+package com.example.e4.rcp.todo.renderer;
+
+import org.eclipse.e4.ui.internal.workbench.swt.AbstractPartRenderer;
+import org.eclipse.e4.ui.model.application.ui.MUIElement;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.workbench.renderers.swt.ContributedPartRenderer;
+import org.eclipse.e4.ui.workbench.renderers.swt.WorkbenchRendererFactory;
+
+public class MyRendererFactory extends WorkbenchRendererFactory {
+
+	private MyMapRenderer mapRenderer;
+
+	@Override
+	public AbstractPartRenderer getRenderer(MUIElement uiElement, Object parent) {
+
+		if (uiElement instanceof MPart) {
+			if (mapRenderer == null) {
+				mapRenderer = new MyMapRenderer();
+				super.initRenderer(mapRenderer);
+			}
+			return mapRenderer;
+		}
+
+		return super.getRenderer(uiElement, parent);
+	}
+
+}
