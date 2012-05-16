cdd7716d656b5483f9b82846735d6b9e408022a1
Lars Vogel <lars.vogel@gmail.com>
2012-05-16 20:01:30 +0200

    BrowserRenderer

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/renderer/MyRendererFactory.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/renderer/MyRendererFactory.java
@@ -8,14 +8,14 @@ import org.eclipse.e4.ui.workbench.renderers.swt.WorkbenchRendererFactory;
 
 public class MyRendererFactory extends WorkbenchRendererFactory {
 
-	private MyMapRenderer mapRenderer;
+	private BrowserRenderer mapRenderer;
 
 	@Override
 	public AbstractPartRenderer getRenderer(MUIElement uiElement, Object parent) {
 
 		if (uiElement instanceof MPart) {
 			if (mapRenderer == null) {
-				mapRenderer = new MyMapRenderer();
+				mapRenderer = new BrowserRenderer();
 				super.initRenderer(mapRenderer);
 			}
 			return mapRenderer;
